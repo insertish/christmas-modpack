32003f56e5ff4d8158de0d872a41a4c4168e6b8f
Paul Makles <paulmakles@gmail.com>
2022-09-02 10:16:23 +0100

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# 2022 Modpack
+
+- [Mod List](mods.md)
+
+## Applied Patches
+
+None yet.
+
+## Additional Configuration
+
+The following should be configured on the server:
+- Disable spawn protection
+- Enforce whitelist if needed
+- Change `max-players`
+- Enable command blocks if required
+- Set `allow-flight` to `true`
---
+++ generate.mjs
@@ -0,0 +1,27 @@
+#!/usr/bin/env zx
+
+import fs from "fs-extra";
+import toml from "toml";
+
+const listing = [];
+
+const mods = await fs.readdir("mods");
+for (const mod of mods) {
+    if (mod.endsWith('.toml')) {
+        const contents = await fs.readFile("mods/" + mod);
+        const data = toml.parse(contents);
+
+        let url;
+        if (mod === 'optifine.toml') {
+            url = "https://optifine.net";
+        } else {
+            url = "https://www.curseforge.com/minecraft/mc-mods/" + mod.split('.')[0];
+        }
+
+        listing.push(`|${data.name}|${data.side}|[CurseForge](${url})|`);
+    }
+}
+
+await fs.writeFile('mods.md',
+    `# ${listing.length} Mods\n\n|Name|Side|\n|---|---|---|\n` + listing.join('\n')
+);
